edc2123ea6f2951a384432ceeea2c763e74acace
Paul Mohr <github@sopamo.de>
2015-04-29 10:08:14 +0200

    Added wakelock

--- app/src/main/java/de/sopamo/uni/sleepminder/Recorder.java
+++ app/src/main/java/de/sopamo/uni/sleepminder/Recorder.java
@@ -1,6 +1,10 @@
 package de.sopamo.uni.sleepminder;
 
 import android.content.Context;
+import android.os.PowerManager;
+import android.util.Log;
+
+import java.io.File;
 
 import de.sopamo.uni.sleepminder.detectors.LightRecorder;
 import de.sopamo.uni.sleepminder.storage.FileHandler;
@@ -15,6 +19,7 @@ public class Recorder {
     private LightRecorder lightRecorder = null;
     private String data = "";
     private String startTime = "";
+    private PowerManager.WakeLock wakeLock;
 
     /**
      * Start tracking
@@ -25,6 +30,10 @@ public class Recorder {
 
         lightRecorder = new LightRecorder();
         lightRecorder.start(context);
+
+        PowerManager mgr = (PowerManager)context.getSystemService(Context.POWER_SERVICE);
+        wakeLock = mgr.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "MyWakeLock");
+        wakeLock.acquire();
 
         // Add the lux information every 5 seconds
         final android.os.Handler customHandler = new android.os.Handler();
@@ -37,12 +46,16 @@ public class Recorder {
                         // We have to check if we already have a "current" lux. In the first call we might not have gotten a sensor change event.
                         if(lightRecorder.getCurrentLux() != null) {
                             data += " " + String.valueOf(lightRecorder.getCurrentLux().intValue());
+                        } else {
+                            Log.e("foo","current lux null");
                         }
                         // Dump the data to the text file if we accumulated "enough"
-                        if(data.length() > 2000) {
+                        if(data.length() > 20) {
                             dumpData();
                         }
                         customHandler.postDelayed(this, 5000);
+                    } else {
+                        Log.e("foo","light recorder null");
                     }
                 }
             }
@@ -63,6 +76,8 @@ public class Recorder {
                 lightRecorder = null;
             }
 
+            wakeLock.release();
+
             // Write the data to a file
             dumpData();
             startTime = "";
@@ -74,6 +89,10 @@ public class Recorder {
      */
     private void dumpData() {
         FileHandler.saveFile(data,"recording-" + startTime + ".txt");
+        Log.e("foo", "dumped data");
+        File file = new File(Application.context.getFilesDir() + "/recording-" + startTime + ".txt");
+        String content = FileHandler.readFile(file);
+        Log.e("foo", content);
         data = "";
     }
 }
