2dc1766987cef5cc68b427d7321b2a403ef6769b
Paul Mohr <github@sopamo.de>
2015-06-19 15:59:14 +0200

    Refactoring

---
+++ sleepminder.lib/src/main/java/de/sopamo/uni/sleepminder/lib/detection/FeatureExtractor.java
@@ -0,0 +1,105 @@
+package de.sopamo.uni.sleepminder.lib.detection;
+
+/**
+ * Created by paulmohr on 19.06.15.
+ */
+public class FeatureExtractor {
+
+    private NoiseModel noiseModel;
+    private float[] lowFreq;
+    private float[] highFreq;
+
+    public FeatureExtractor(NoiseModel noiseModel) {
+
+        this.noiseModel = noiseModel;
+    }
+
+    public void update(short[] buffer) {
+        lowFreq = new float[buffer.length];
+        highFreq = new float[buffer.length];
+        noiseModel.addRLH(calculateRLH(buffer));
+        noiseModel.addRMS(calculateRMS(buffer));
+        noiseModel.addVAR(calculateVar(buffer));
+
+        noiseModel.calculateFrame();
+    }
+
+
+    private double calculateRMS(short[] buffer) {
+        long sum = 0;
+        for(int i=0;i<buffer.length;i++) {
+            sum += Math.pow(buffer[i],2);
+        }
+        return Math.sqrt(sum / buffer.length);
+    }
+
+    private double calculateRMS(float[] buffer) {
+        long sum = 0;
+        for(int i=0;i<buffer.length;i++) {
+            sum += Math.pow(buffer[i],2);
+        }
+        return Math.sqrt(sum / buffer.length);
+    }
+
+    private double calculateLowFreqRMS(short[] buffer) {
+        lowFreq[0] = 0;
+
+        float a = 0.25f;
+
+        for(int i=1;i<buffer.length;i++) {
+            lowFreq[i] = lowFreq[i-1] + a * (buffer[i] - lowFreq[i-1]);
+        }
+
+        return calculateRMS(lowFreq);
+    }
+
+    private double calculateHighFreqRMS(short[] buffer) {
+        highFreq[0] = 0;
+
+        float a = 0.25f;
+
+        for(int i=1;i<buffer.length;i++) {
+            highFreq[i] = a * (highFreq[i-1] + buffer[i] - buffer[i-1]);
+        }
+
+        return calculateRMS(highFreq);
+    }
+
+    private double calculateRLH(short[] buffer) {
+        double rmsh = calculateHighFreqRMS(buffer);
+        double rmsl = calculateLowFreqRMS(buffer);
+        if(rmsh == 0) return 0;
+        if(rmsl == 0) return 0;
+        return  rmsl / rmsh;
+    }
+
+    /**
+     * Calculates the var of one frame
+     *
+     * @param buffer
+     * @return
+     */
+    private double calculateVar(short[] buffer) {
+
+        double mean = calculateMean(buffer);
+        double var = 0;
+        for(short s: buffer) {
+            var += Math.pow(s - mean,2);
+        }
+        return var / buffer.length;
+    }
+
+    /**
+     * Calculate the mean of one fram
+     *
+     * @param buffer
+     * @return
+     */
+    private double calculateMean(short[] buffer) {
+        double mean = 0;
+        for(short s: buffer) {
+            mean += s;
+        }
+        return mean / buffer.length;
+    }
+}
--- sleepminder.lib/src/main/java/de/sopamo/uni/sleepminder/lib/recorders/AudioRecorder.java
+++ sleepminder.lib/src/main/java/de/sopamo/uni/sleepminder/lib/recorders/AudioRecorder.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import java.io.IOException;
 
 import de.sopamo.uni.sleepminder.lib.DebugView;
+import de.sopamo.uni.sleepminder.lib.detection.FeatureExtractor;
 import de.sopamo.uni.sleepminder.lib.detection.NoiseModel;
 
 public class AudioRecorder extends Thread {
@@ -18,11 +19,8 @@ public class AudioRecorder extends Thread {
     private static int N = 0;
     private NoiseModel noiseModel;
     private DebugView debugView;
-    private float[] lowFreq;
-    private float[] highFreq;
     private short[] buffer;
-    private short[] fullbuffer;
-    MediaRecorder mRecorder;
+    private FeatureExtractor featureExtractor;
 
 
     public AudioRecorder(NoiseModel noiseModel) {
@@ -33,6 +31,7 @@ public class AudioRecorder extends Thread {
 
         this.noiseModel = noiseModel;
         this.debugView = debugView;
+        this.featureExtractor = new FeatureExtractor(noiseModel);
     }
 
     @Override
@@ -45,10 +44,8 @@ public class AudioRecorder extends Thread {
     private void capture() {
         int i = 0;
         android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_URGENT_AUDIO);
-        if(lowFreq == null || highFreq == null || buffer == null) {
+        if(buffer == null) {
             buffer  = new short[1600];
-            lowFreq = new float[buffer.length];
-            highFreq = new float[buffer.length];
         }
 
         if(N == 0) {
@@ -75,11 +72,7 @@ public class AudioRecorder extends Thread {
 
     private void process(short[] buffer) {
 
-        noiseModel.addRLH(calculateRLH(buffer));
-        noiseModel.addRMS(calculateRMS(buffer));
-        noiseModel.addVAR(calculateVar(buffer));
-
-        noiseModel.calculateFrame();
+        featureExtractor.update(buffer);
 
         if(debugView != null) {
             debugView.addPoint2(noiseModel.getNormalizedRLH(), noiseModel.getNormalizedVAR());
@@ -95,83 +88,6 @@ public class AudioRecorder extends Thread {
 
     }
 
-    private double calculateRMS(short[] buffer) {
-        long sum = 0;
-        for(int i=0;i<buffer.length;i++) {
-            sum += Math.pow(buffer[i],2);
-        }
-        return Math.sqrt(sum / buffer.length);
-    }
-
-    private double calculateRMS(float[] buffer) {
-        long sum = 0;
-        for(int i=0;i<buffer.length;i++) {
-            sum += Math.pow(buffer[i],2);
-        }
-        return Math.sqrt(sum / buffer.length);
-    }
-
-    private double calculateLowFreqRMS(short[] buffer) {
-        lowFreq[0] = 0;
-
-        float a = 0.25f;
-
-        for(int i=1;i<buffer.length;i++) {
-            lowFreq[i] = lowFreq[i-1] + a * (buffer[i] - lowFreq[i-1]);
-        }
-
-        return calculateRMS(lowFreq);
-    }
-
-    private double calculateHighFreqRMS(short[] buffer) {
-        highFreq[0] = 0;
-
-        float a = 0.25f;
-
-        for(int i=1;i<buffer.length;i++) {
-            highFreq[i] = a * (highFreq[i-1] + buffer[i] - buffer[i-1]);
-        }
-
-        return calculateRMS(highFreq);
-    }
-
-    private double calculateRLH(short[] buffer) {
-        double rmsh = calculateHighFreqRMS(buffer);
-        double rmsl = calculateLowFreqRMS(buffer);
-        if(rmsh == 0) return 0;
-        if(rmsl == 0) return 0;
-        return  rmsl / rmsh;
-    }
-
-    /**
-     * Calculates the var of one frame
-     *
-     * @param buffer
-     * @return
-     */
-    private double calculateVar(short[] buffer) {
-
-        double mean = calculateMean(buffer);
-        double var = 0;
-        for(short s: buffer) {
-            var += Math.pow(s - mean,2);
-        }
-        return var / buffer.length;
-    }
-
-    /**
-     * Calculate the mean of one fram
-     *
-     * @param buffer
-     * @return
-     */
-    private double calculateMean(short[] buffer) {
-        double mean = 0;
-        for(short s: buffer) {
-            mean += s;
-        }
-        return mean / buffer.length;
-    }
 
     public void close() {
         stopped = true;
